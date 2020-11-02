273a670d6cf796bafa7b3abd13e2dc215ae8fdab
Md. Rashadul Islam <rashadpust@gmail.com>
2020-11-03 04:45:36 +0600

    Unused variables removed

--- src/components/category/add-category.jsx
+++ src/components/category/add-category.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
 
 import { showCategoryModal} from '../../redux/action';
 
